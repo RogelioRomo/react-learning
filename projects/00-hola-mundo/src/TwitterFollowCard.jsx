cc98cd5807df98f66e7e568b44f54ad90a19de90
rogelioromo <rogelioromo25@gmail.com>
2023-11-24 16:57:23 -0800

    TwitterFollowCard project finalized

--- projects/00-hola-mundo/src/TwitterFollowCard.jsx
+++ projects/00-hola-mundo/src/TwitterFollowCard.jsx
@@ -34,10 +34,22 @@ por lo que la mejor manera es dandole parametros, una funcion que
 recibe parametros siempre se puede reutilizar */
 
 import "./TwitterFollowCard.css"
+import { useState } from "react"
 
-export function TwitterFollowCard({ userName, children, isFollowing }) {
-    const imageSrc= `https://unavatar.io/twitter/${userName}`
+export function TwitterFollowCard({ userName, children }) {
+    const [isFollowing, setIsFollowing] = useState(false)
+
     
+    const imageSrc= `https://unavatar.io/twitter/${userName}`
+    const text = isFollowing ? "Siguiendo" : "Seguir"
+    const buttonClassName = isFollowing 
+    ? "tw-followCard-button is-following"
+    : "tw-followCard-button"
+
+    const handleClick = () => {
+        setIsFollowing(!isFollowing)
+    }
+
     return (
         <article className="tw-followCard">
             <header className="tw-followCard-header">
@@ -51,15 +63,27 @@ export function TwitterFollowCard({ userName, children, isFollowing }) {
             </header>
 
             <aside>
-                <button className="tw-followCard-button">Follow</button>
+                <button className={ buttonClassName } onClick={handleClick}>{ text }</button>
             </aside>
         </article>
     )
 }
 
-/* Notese que para la imagen, lo que podemos hacer es dejarlo
-declarado como una constante la cual recibira el userName y 
-de esa manera el URL se hace dinamico y reutilizable. Podriamos
-evitar la linea de creacion de la constante simplemente poniendo
-de la siguiente manera entre corchetes (importante eso)
-<img className="tw-followCard-img" src={`https://unavatar.io/soundcloud/${userName}`} alt="user name avatar" />*/
+/* 
+Notese que para la imagen, lo que podemos hacer es dejarlo declarado como una constante la cual recibira el userName y de esa manera el URL se hace dinamico y reutilizable. Podriamos
+evitar la linea de creacion de la constante simplemente poniendo de la siguiente manera entre corchetes (importante eso)
+<img className="tw-followCard-img" src={`https://unavatar.io/soundcloud/${userName}`}alt="user name avatar" />
+
+Mas puntos a notar.
+Las constantes 'text' y 'buttonClassName' las estamos implementando para hacer que la interfaz de usuario
+sea mas dinamica, esto pues, estamos queriendo implementar estados en donde dependiendo de este mismo
+el texto del boton cambie de Seguir a Siguiendo o viceversa, lo mismo con los estilos que definimos en buttonClassName
+
+El estado que decidimos usar es useState ya que con este hook lo que podemos hacer es nombrar variables que definan
+algun estado que nosotros queramos. Por convencion cuando se usa este hook usamos destructuracion para nombrar a las variables
+por eso es que en nuestro ejemplo [isFollowing, setIsFollowing] esta entre llaves.  
+
+Al final creamos 'handleClick' para declarar con una funcion que cuando el 'onClick' se ejecute, 
+este ejecute esta funcion y la funcion va a retornar setIsFollowing evaluando lo contrario de 'isFollowing'.
+
+*/
